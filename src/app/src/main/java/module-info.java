38cb1ae1645ddac0570abf5588d67d0287322b01
kevinza <fkevinza@gmail.com>
2025-06-11 16:06:00 +0700

    fix: Add missing 'requires org.json' in module-info

--- src/app/src/main/java/module-info.java
+++ src/app/src/main/java/module-info.java
@@ -2,5 +2,8 @@ module com.owo {
     requires javafx.controls;
     requires javafx.web;
     requires jdk.jsobject;
+    requires javafx.graphics;
+    requires org.json;
+
     exports com.owo;
 }
